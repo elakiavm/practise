7337c499d35ff9e74b5ac816fa57da4b209a659a
elakia <vmelakia@gmail.com>
2022-02-28 20:03:52 +0530

    update the file

--- Application.java
+++ Application.java
@@ -25,8 +25,8 @@ public class Application
         boolean result1 = first.equals(second);
         System.out.println("Strings first and second are equal: " + result1);
 
-        boolean result2 = first.equals(line.);
-        System.out.println("Strings first and second are equal: " + result2);
+        // boolean result2 = first.equals(line.);
+        // System.out.println("Strings first and second are equal: " + result2);
         
         System.out.println(line.toLowerCase());
         
